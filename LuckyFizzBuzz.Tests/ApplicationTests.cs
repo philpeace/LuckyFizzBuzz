d69bccc820576d5d6b5f673e7ae9c81fce687853
Phil <philip.peace@drfoster.co.uk>
2017-03-18 17:33:00 +0000

    Step 2: luckyfizzbuzz

--- LuckyFizzBuzz.Tests/ApplicationTests.cs
+++ LuckyFizzBuzz.Tests/ApplicationTests.cs
@@ -16,13 +16,13 @@ namespace LuckyFizzBuzz.Tests
         [Theory]
         [InlineData("1", "1")]
         [InlineData("1 2", "1 2")]
-        [InlineData("1 2 3", "1 2 fizz")]
+        [InlineData("1 2 3", "1 2 lucky")]
         [InlineData("1 2 6 18", "1 2 fizz fizz")]
-        [InlineData("1 2 3 4 5", "1 2 fizz 4 buzz")]
-        [InlineData("1 2 3 4 5 6 7 8 9 10", "1 2 fizz 4 buzz fizz 7 8 fizz buzz")]
+        [InlineData("1 2 3 4 5", "1 2 lucky 4 buzz")]
+        [InlineData("1 2 3 4 5 6 7 8 9 10", "1 2 lucky 4 buzz fizz 7 8 fizz buzz")]
         [InlineData("1 2 15", "1 2 fizzbuzz")]
-        [InlineData("1 2 15 18 30", "1 2 fizzbuzz fizz fizzbuzz")]
-        [InlineData("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20", "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz")]
+        [InlineData("1 2 15 18 30", "1 2 fizzbuzz fizz lucky")]
+        [InlineData("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20", "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz")]
         public void ApplicationRunReturnsCorrectValue(string input, string expected)
         {
             var app = new Application();
--- LuckyFizzBuzz/Application.cs
+++ LuckyFizzBuzz/Application.cs
@@ -8,29 +8,37 @@ namespace LuckyFizzBuzz
     {
         public string Run(string input)
         {
-            var numbers = input.Split(' ').Select(int.Parse).ToArray();
+            var numbers = input.Split(' ');
             var output = new List<string>();
 
-            foreach (var number in numbers)
+            foreach (var rawNumber in numbers)
             {
+                var number = int.Parse(rawNumber);
                 var isFizz = number % 3 == 0;
                 var isBuzz = number % 5 == 0;
 
-                if (isFizz && isBuzz)
+                if (rawNumber.Contains("3"))
                 {
-                    output.Add("fizzbuzz");
-                }
-                else if (isFizz)
-                {
-                    output.Add("fizz");
-                }
-                else if (isBuzz)
-                {
-                    output.Add("buzz");
+                    output.Add("lucky");
                 }
                 else
                 {
-                    output.Add(number.ToString());                   
+                    if (isFizz && isBuzz)
+                    {
+                        output.Add("fizzbuzz");
+                    }
+                    else if (isFizz)
+                    {
+                        output.Add("fizz");
+                    }
+                    else if (isBuzz)
+                    {
+                        output.Add("buzz");
+                    }
+                    else
+                    {
+                        output.Add(number.ToString());                   
+                    }
                 }
             }
 
